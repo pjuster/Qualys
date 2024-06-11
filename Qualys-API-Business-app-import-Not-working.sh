87f63a4d70ded63bda876c2bc08b253fc84d6bb3
pjuster <88379467+pjuster@users.noreply.github.com>
2024-06-11 12:36:52 +0100

    Qualys-API-Business-app-import.sh

--- Qualys-API-Business-app-import-Not-working.sh
+++ Qualys-API-Business-app-import-Not-working.sh
@@ -7,30 +7,28 @@
 
 token=$(curl -s -X POST "https://gateway.qg1.apps.qualys.eu/auth" -H "Content-Type: application/x-www-form-urlencoded" -d "username=quays-pu8&password=FMrKPRhI&token=true&permissions=true")
 
-curl --location 'https://gateway.qg1.apps.qualys.eu/rest/2.0/update/am/asset/business/metadata' \
+curl --location 'https://gateway.qg1.apps.qualys.eu/rest/2.0/upsert/am/businessapp/metadata' \
 --header 'X-Requested-With: QualysPostman' \
 --header 'Content-Type: application/json' \
 --header "Authorization:bearer ${token}" \
 --data '
 {
  "data": [
- {
- "businessAppId": "2fc86c650a0a0bb4003698b5331640df",
- "name": "PJ - Banking Service",
- "businessCriticality": "1 - Most Critical",
- "status": "Installed",
- "environment": "Production",
- "usedFor": "Production",
- "created": 1620643264000,
- "lastUpdated": 1620653309000,
- "operationalStatus": "Installed",
- "ownedBy": "Joey Bolick",
- "managedBy": "Byron Fortuna",
- "supportedBy": "John Doe",
- "supportGroup": "IT Operations"
- }
- ]
+ 			{
+ 				"businessAppId": "2fc86c650a0a0bb4003698b5331640df",
+ 				"name": "Banking Service",
+				"businessCriticality": "1 - Most Critical",
+ 				"status": "Installed",
+				"environment": "Production",
+				"usedFor": "Production",
+ 				"created": 1620643264000,
+ 				"lastUpdated": 1620653309000,
+ 				"operationalStatus": "Installed",
+ 				"ownedBy": "Joey Bolick",
+ 				"managedBy": "Byron Fortuna",
+ 				"supportedBy": "John Doe",
+ 				"supportGroup": "IT Operations"
+ 			}
+		 ]
 }'
 
-#pjuster@136194-MBP Documents % ./Qualys-API-Business-app-import.sh 
-#{"requestId":"f9fef0e8-02ac-4dca-9e19-43be38fd1a16","responseMessage":"Failed to import asset metadata","responseCode":"FAILED","failedIds":["null"]}%          pjuster@136194-MBP Documents % 
